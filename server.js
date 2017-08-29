1e274d542a1b6b35d7110fd73bb7e77eb527814a
unknown <andrenns@gmail.com>
2017-08-29 12:59:01 -0300

    acertando os arquivos para o heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express =  require('express');
 
 var app = express();
-var server = app.listen(3000);
+//var server = app.listen(3000);
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static('public'));
 
