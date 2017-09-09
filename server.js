69d44876626c546e7d723468251d0fe68c7ac38e
unknown <andrenns@gmail.com>
2017-09-09 15:34:03 -0300

    acertando o heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express =  require('express');
-
 var app = express();
+
 //var server = app.listen(3000);
 app.set('port', (process.env.PORT || 3000));
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 console.log("My socket server is runnig");
 
